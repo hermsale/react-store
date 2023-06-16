ef251c4f8a80537e29d66e8fa83cf1a345d5d110
Alejandro Hugo Herms <hermsale@gmail.com>
2023-06-16 17:42:10 -0300

    se empezo a trabajar en la page MyOrders

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,12 +1,29 @@
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
 import Layout from "../../Components/Layout"
+import { Link } from "react-router-dom"
+import OrdersCard from "../../Components/OrdersCard"
 
 function MyOrders() {
+
+  const context = useContext(ShoppingCartContext)
+
     return (
       <Layout>
-        Hola MyOrders
+        <div className="flex items-center justify-between relative w-80">
+          <h1>My Orders</h1>
+        </div>
+        {
+          context.order.map((order, index)=>{
+            <Link key={index} to={`/my-orders/${order.id}`}>
+              <OrdersCard
+                totalPrice={order.totalPrice}
+                totalProducts={order.totalProducts}
+              />
+            </Link>
+          })
+        }
       </Layout>
-     
-  
     )
   }
   
